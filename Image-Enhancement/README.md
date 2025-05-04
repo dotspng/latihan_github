7e4efdc5fbb2644eb6b579dfbcc92651e0b51fa0
dotspng <155881804+dotspng@users.noreply.github.com>
2025-05-04 14:23:16 +0700

    Add files via upload
    Image Enhancement
    
    Proses peningkatan mutu citra ini termasuk memperbaiki citra yang ketika proses akuisisi mengalami ganguan yang signifikan seperti noise, gangguan geometris, radiometrik dan beberapa gangguan faktor alam lainnya.
    
    fitur-fitur
    histogram equalization
    rgb2gray
    contrast streching
    noise salt and pepper
    median filter
    histogra histeq
    histogram streching

---
+++ Image-Enhancement/README.md
@@ -0,0 +1,34 @@
+# Image Enhancement Using MatLab
+
+This project uses different algorithms and built-in functions in Matlab for Image Enhancement like: RGB2Gray, Histogram Equalization, Histogram Stretching, Median and average filtering. 🚀👩‍🚀
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+```
+MatLab 2015
+```
+
+### Installing
+
+```
+Just a normal installing for any version of MatLab
+```
+
+## Built With
+
+* [MatLab](https://www.mathworks.com/products/matlab.html) 
+
+## Contributing
+
+Anyone is more than welcome to contribute , just take a clone and you know the rest 😅😉
+
+## Authors
+
+* **Ahmed Adel Fahmy** - *Initial work* 
+
+
+
---
+++ Image-Enhancement/untitled.m
@@ -0,0 +1,190 @@
+function varargout = untitled(varargin)
+% UNTITLED MATLAB code for untitled.fig
+%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%
+%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled
+
+% Last Modified by GUIDE v2.5 25-Dec-2018 12:36:51
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled is made visible.
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% Choose default command line output for untitled
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global a;
+[filename,path]= uigetfile('*.jpg', 'Select image');
+fullpath = fullfile(path,filename);
+a = imread(fullpath);
+axes(handles.axes1);
+imshow(a);
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%changes the rgb to a gray one
+global a;
+global gray 
+gray = rgb2gray(a);
+axes(handles.axes2);
+imshow(gray);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global gray;
+histogram1 = histogram(gray, 'Parent', handles.axes3);
+%axes(handles.axes3);
+imshow(histogram1);
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global gray;
+global histeqimg;
+histeqimg = histeq(gray);
+axes(handles.axes4);
+imshow(histeqimg);
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global gray;
+global histStretching; 
+histStretching = imadjust(gray , [0.3, 0.6] , [0.0 , 1]);
+axes(handles.axes5);
+imshow(histStretching);
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global gray;
+global noisyImg;
+noisyImg = imnoise(gray , 'salt & pepper' , 0.025);
+axes(handles.axes6);
+imshow(noisyImg);
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global noisyImg;
+kaverage = filter2(fspecial('average',3),noisyImg)/255;
+axes(handles.axes7);
+imshow(kaverage);
+
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global noisyImg;
+noisyImgDouble = im2double(noisyImg);
+medianFilter = medfilt1(noisyImgDouble);
+axes(handles.axes8);
+imshow(medianFilter);
+
+
+% --- Executes on button press in pushbutton9.
+function pushbutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global histeqimg;
+%global histeq;
+histequ = histogram(histeqimg , 'Parent' , handles.axes9);
+imshow(histequ);
+
+
+% --- Executes on button press in pushbutton10.
+function pushbutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global histStretching; 
+strechedHist = histogram(histStretching , 'Parent' , handles.axes10);
+imshow(strechedHist)
